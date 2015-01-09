317c91aa3da65de65eb69ac1b26d6a0f7cec74a1
Peter Lisy <peter.lisy@sk.ibm.com>
2015-01-09 10:59:19 +0100

    turned into express app

--- index.js
+++ index.js
@@ -4,41 +4,10 @@ var express = require('express');
 
 var app = express();
 
-/*
-application: polymer-designer
- version: 1
- runtime: python27
- api_version: 1
- threadsafe: yes
-
- handlers:
- - url: /
- static_files: build.html
- upload: build.html
-
- - url: /build.js
- static_files: build.js
- upload: build.js
-
- - url: /preview.html
- static_files: preview.html
- upload: preview.html
-
- - url: /components
- static_dir: components
-
- - url: /elements
- static_dir: elements
-
- - url: /assets
- static_dir: assets
-
-*/
-
 app.use('/', express.static(path.resolve('./')));
 
 
-var port = process.env.PORT || 8000;
+var port = process.env.VCAP_APP_PORT ||  process.env.PORT || 8000;
 
 app.listen(port, function () {
     console.log('App started at http://localhost:' + port + '/');
