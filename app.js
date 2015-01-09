33ebf7ba32f1b3cbd4284bef789d541984407583
Peter Lisy <peter.lisy@sk.ibm.com>
2015-01-09 14:21:25 +0100

    added component upload

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 
 var app = express();
 
 app.use('/', express.static(path.resolve('./')));
+app.use(bodyParser.json());
+
+
+app.post('/files/:fileId', function (req, res) {
+
+    if (req.params.fileId) {
+        var filesFolder = './files/';
+
+        if (!fs.existsSync(filesFolder)) {
+            fs.mkdirSync(filesFolder);
+        }
+
+        var fileName  = filesFolder + req.params.fileId + '.html';
+
+        fs.writeFile(fileName, req.body.content, function (err) {
+
+            if (err) {
+                res.statusCode = 500;
+                res.json({
+                    message: 'server error',
+                    error: err
+                });
+                return;
+            }
+
+            res.send('OK');
+
+        });
+
+        return;
+
+    }
+
+    res.statusCode = 400;
+    res.send('bad request');
+
+});
 
 
 var port = process.env.VCAP_APP_PORT ||  process.env.PORT || 8000;
